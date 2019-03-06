bc64bcf9570139f877d7d0cc1acdaa1cdfae21c5
Côme Chicoteau <cchicoteau@MacBook-Pro-de-Come.local>
2019-03-06 20:42:44 +0100

    working ft_strcat

--- src/ft_strcat.s
+++ src/ft_strcat.s
@@ -7,13 +7,42 @@
 
 section	.text
 	global		_ft_strcat
-	extern		_printf
+
+_get_dest_len:
+	mov		rcx, 0
+	loop1:
+		cmp	byte [r8 + rcx], 0
+		je	ret_process
+		inc	rcx
+		jmp	loop1
+	ret_process:
+		ret
+
+_get_src_len:
+	mov		rcx, 0
+	loop2:
+		cmp	byte [r9 + rcx], 0
+		je	ret_process
+		inc	rcx
+		jmp	loop2
 
 _ft_strcat:
 	push		rbp
 	mov		rbp, rsp
 
-	mov		rax, rdi
-	
-	leave
-	ret
+	mov		r8, rdi
+	mov		r9, rsi
+	call		_get_dest_len
+	cld
+	lea		rdx, [rdi + rcx]
+	call		_get_src_len
+	mov		rdi, rdx
+	rep		movsb
+	call		_get_dest_len
+	sub		rdi, rcx
+	mov		byte [rdi + rcx], 0
+
+	exit_process:
+		mov	rax, rdi
+		leave
+		ret
--- tests/test_strcat.c
+++ tests/test_strcat.c
@@ -6,7 +6,7 @@
 /*   By: cchicote <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/06 02:42:15 by cchicote          #+#    #+#             */
-/*   Updated: 2019/03/06 03:54:28 by cchicote         ###   ########.fr       */
+/*   Updated: 2019/03/06 20:35:22 by cchicote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,25 @@
 
 int print_tests_strcat = 1;
 
+void			strcat_example(char *s1, char *s2)
+{
+	printf("strcat: [%s]\n", strcat(s1, s2));
+}
+
 int			test_strcat(void)
 {
 	char		*s1;
 	char		*s2;
 	int		ret;
 
-	s1 = strdup("Bonjour    ");
-	s2 = strdup("        abc");
+	s1 = (char*)malloc(sizeof(char) * 100);
+	bzero(s1, 100);
+	strcpy(s1, "on a de quoi faire");
+	s2 = strdup("aaaaa yolo");
 	ret = 0;
 	if (print_tests_strcat)
 	{
+		strcat_example(strdup(s1), strdup(s2));
 		printf("ft_strcat: [%s]\n", ft_strcat(s1, s2));
 	}
 	return (ret);
