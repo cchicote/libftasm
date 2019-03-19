edc013135824fec4ff7014c31477b50b2f837e37
Côme Chicoteau <cchicoteau@MacBook-Pro-de-Come.local>
2019-03-19 21:24:07 +0100

    better comments in ft_memalloc

--- src/ft_memalloc.s
+++ src/ft_memalloc.s
@@ -19,21 +19,21 @@ _ft_memalloc:
 	mov			rax, 0					; set rax to NULL
 
 	cmp			rdi, 0					; if size is zero
-	je			exit_process			; do nothing and return NULL
+	je			exit_process			; do nothing and return NULL (rax value)
 
 	mov			[rbp - 8], rdi			; save size in stack frame
 
-	call		_malloc					; 
-	test		rax, rax				; 
-	js			exit_process			; 
+	call		_malloc					; malloc the size asked
+	test		rax, rax				; check malloc return
+	js			exit_process			; if malloc returns NULL then return NULL
 
-	mov			[rbp - 16], rax
+	mov			[rbp - 16], rax			; save malloc-ed zone into stack frame
 
-	mov			rsi, [rbp - 8]
-	mov			rdi, [rbp - 16]
-	call		_ft_bzero
+	mov			rsi, [rbp - 8]			; prepare arguments for ft_bzero (size)
+	mov			rdi, [rbp - 16]			; (zone)
+	call		_ft_bzero				;
 
-	mov			rax, [rbp - 16]
+	mov			rax, [rbp - 16]			; return the malloc-ed zone
 
 	exit_process:						; exit marker
 		leave							; leave stack frame
